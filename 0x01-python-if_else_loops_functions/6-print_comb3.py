191586740990abe665e93b870f8367d704d568b3
Winebald250 <winebaldbanituze@gmail.com>
2024-01-05 05:09:42 -0800

    python loops

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-
-for i in range(0, 10):
-    for j in range(1, 10):
-        if i >= j:
-            continue
-        if i == 8 and j == 9:
-            print("{:d}{:d}".format(i, j))
+for num in range(0, 90):
+    if num % 10 > num / 10:
+        if num != 89:
+            print("{:02d}, ".format(num), end='')
         else:
-            print("{:d}{:d}".format(i, j), end=", ")
+            print("{:02d}".format(num))
